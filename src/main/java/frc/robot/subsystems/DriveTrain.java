4a90917f4d9d841b433a9ca59f13f146372ff04d
John Koehring <john@koehring.com>
2019-09-21 11:54:35 -0700

    updating drive train to automatically use gamepad

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -63,7 +63,7 @@ public class DriveTrain extends Subsystem
 	public void initDefaultCommand()
 	{
 		// Set the default command for a subsystem here.
-		//.setDefaultCommand(new DriveWithJoysticks());
+		setDefaultCommand(new DriveWithGamepadController());
 	}
 	
 	/**
